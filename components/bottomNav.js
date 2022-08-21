e1f6583087a3716f49a33183b5066b67cb778f0d
Joel Velis <joelvelis94@gmail.com>
2022-08-20 21:10:32 -0400

    set up app and start index page

---
+++ components/bottomNav.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleExclamation, faCircleQuestion, faHouse, faMap, faBars, faQuestion, faSign, faWifi } from '@fortawesome/free-solid-svg-icons'
+
+export default function BottomNav() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <div className={styles.flexContainer}>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+            <FontAwesomeIcon icon={faBars} style={{ fontSize: 28}}/>
+        </a>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+            <FontAwesomeIcon icon={faWifi} style={{ fontSize: 28}}/>
+        </a>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+            <FontAwesomeIcon icon={faMap} style={{ fontSize: 28}}/>
+        </a>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+            <FontAwesomeIcon icon={faCircleExclamation} style={{ fontSize: 28}}/>
+        </a>
+        <a href="https://nextjs.org/docs" className={styles.card}>
+            <FontAwesomeIcon icon={faCircleQuestion} style={{ fontSize: 28}}/>
+        </a>
+    </div>
+  );
+}
