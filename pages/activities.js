384b31c22d024642c355bf87d1195a2697da5bfc
Joel Velis <joelvelis94@gmail.com>
2022-09-02 21:37:43 -0400

    create nav bar for activities;

--- pages/activities.js
+++ pages/activities.js
@@ -1,18 +1,25 @@
+import { useState } from 'react'
+import styles from '../styles/Activities.module.css'
+
 export default function Activities({data}) {
+    const [activeActivity, setActiveActivity] = useState('bars')
     const activities = data.activities
     console.log(activities)
+    console.log(activities.bars)
+
+
+
     return (
         <div>
             <div>
-                <h3>Activies</h3>
+                <h3 className={styles.pageHeader}>Activies</h3>
             </div>
-            <div>
-                <ul>
-                    {activities.forEach(activity => {
-                        return (
-                            <li>{activity}</li>
-                        )
-                    })}
+            <div className={styles.activitiesContainer}>
+                <ul className={styles.activitiesNav}>
+                    <li className={activeActivity == 'bars' ? styles.activeTab : ''} onClick={() => setActiveActivity('bars')}>Bars</li>
+                    <li className={activeActivity == 'restaurants' ? styles.activeTab : ''} onClick={() => setActiveActivity('restaurants')}>Restaurants</li>
+                    <li className={activeActivity == 'outdoors' ? styles.activeTab : ''} onClick={() => setActiveActivity('outdoors')}>Outdoors</li>
+                    <li className={activeActivity == 'tours' ? styles.activeTab : ''} onClick={() => setActiveActivity('tours')}>Tours</li>
                 </ul>
             </div>
         </div>
@@ -20,38 +27,20 @@ export default function Activities({data}) {
 }
 
 export async function getServerSideProps() {
-    // const fs = require("fs");
-    // const { parse } = require("csv-parse");
-    
-    // async function readFile(path) {
-    //     return new Promise((resolve, reject) => {
-    //       fs.readFile(path, 'utf8', function (err, data) {
-    //         parse(data, {columns: false, trim: true}, function(err, rows) {
-    //             if (err) {
-    //                 reject(err);
-    //             }
-    //             resolve(rows);
-    //         })
-    //       });
-    //     });
-    // }
-    // let bar_data = await readFile("./public/files/bars.csv");
     const bars_data = await (await fetch('http://localhost:3000/api/bars-sheet')).json()
     const restaurants_data = await (await fetch('http://localhost:3000/api/restaurants-sheet')).json()
-    const tours_data = await (await fetch('http://localhost:3000/api/tours-sheet')).json()
     const outdoors_data = await (await fetch('http://localhost:3000/api/outdoors-sheet')).json()
+    const tours_data = await (await fetch('http://localhost:3000/api/tours-sheet')).json()
 
     return {
         props: {
             data: {
-                activities: [
-                    {
-                        bars: bars_data.data,
-                        restaurants: restaurants_data.data,
-                        outdoor: tours_data.data,
-                        tours: tours_data.data
-                    }
-                ]
+                activities: {
+                    bars: bars_data.data,
+                    restaurants: restaurants_data.data,
+                    outdoors: outdoors_data.data,
+                    tours: tours_data.data
+                }
             }
         }
     }
