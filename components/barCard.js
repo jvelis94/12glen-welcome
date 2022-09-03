314a684f1346908f8a45cf8a63c74f341acf74d5
Joel Velis <joelvelis94@gmail.com>
2022-09-02 22:50:05 -0400

    create card for each activity

---
+++ components/barCard.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import styles from '../styles/ActivityResults.module.css'
+import { useRouter } from "next/router";
+
+
+export default function BarCard(props) {
+    return (
+        <div className={styles.cardProduct}>
+            <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
+            <div className="cardProductInfos">
+                <h2>Product name</h2>
+                <p>Product description with <strong>relevant info</strong> only.</p>
+            </div>
+        </div>
+    )
+}
